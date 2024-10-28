1005e69b8315e9c94be14a94622f329d25ffe248
Marcos Vinicius <107082481+euumarcosz@users.noreply.github.com>
2024-10-28 07:39:05 -0300

    Renomeados

--- BATMAN.bat
+++ BATMAN.bat
@@ -1,17 +1,17 @@
 @echo off
 
-REM Abrir Google Chrome com várias abas
+REM Abrir Google Chrome utilitários básicos
 start chrome "https://portalwfm.hapvida.com.br/" "http://web.whatsapp.com" "https://outlook.office365.com/mail/" "https://hapvida-my.sharepoint.com/:x:/g/personal/andrezasp_hapvida_com_br/ESwt5xJgPwNJnLUqGB654pkBFc5Yd5-rnm9yIzSsybL72A?rtime=_8p64G3k3Eg" "https://hapvida-my.sharepoint.com/:x:/g/personal/gerson_junior_hapvida_com_br/EU2yV3mq7WZLpRp-Kvi5q7EBVLrWJ-cHvcVEk6lWyRktyA?e=psXYZR"
 
-REM Abrir Firefox com várias abas
+REM Abrir Firefox tela do sistema e tabela!
 start firefox  "http://sistemas.hapvida.com.br/planos" "http://sistemas.hapvida.com.br/planos" "http://www3.hapvida.com.br/hapvida/showFpd.php?idcat=74&id=3215"
 
-REM Abrir aplicativos
+REM Abrir aplicativos essenciais
 start notepad 
 start notepad++
 start mspaint
 
-REM Abrir arquivo em LibreOffice
+REM Abrir arquivo em LibreOffice 
 start start "" "C:\Program Files\LibreOffice\program\soffice.exe"
 
 
