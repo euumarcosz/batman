fc1b15a5b6539c79e75b1ac651fe00100ae454ce
Marcos Vinicius <107082481+euumarcosz@users.noreply.github.com>
2024-11-04 06:43:50 -0300

    Chrome & Firefox, Links

--- BATMAN.bat
+++ BATMAN.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM Abrir Google Chrome utilitários básicos
-start chrome "https://portalwfm.hapvida.com.br/" "http://web.whatsapp.com" "https://outlook.office365.com/mail/" "https://hapvida-my.sharepoint.com/:x:/g/personal/andrezasp_hapvida_com_br/ESwt5xJgPwNJnLUqGB654pkBFc5Yd5-rnm9yIzSsybL72A?rtime=_8p64G3k3Eg" "https://hapvida-my.sharepoint.com/:x:/g/personal/gerson_junior_hapvida_com_br/EU2yV3mq7WZLpRp-Kvi5q7EBVLrWJ-cHvcVEk6lWyRktyA?e=psXYZR"
+start chrome "https://portalwfm.hapvida.com.br/" "http://web.whatsapp.com" "https://outlook.office365.com/mail/" "https://hapvida-my.sharepoint.com/:x:/g/personal/andrezasp_hapvida_com_br/ESwt5xJgPwNJnLUqGB654pkBFc5Yd5-rnm9yIzSsybL72A?rtime=_8p64G3k3Eg" "https://www.google.com.br/maps/@-3.7257216,-38.535168,15z?entry=ttu&g_ep=EgoyMDI0MTAyOS4wIKXMDSoASAFQAw%3D%3D" "https://hapvida-my.sharepoint.com/:x:/g/personal/gerson_junior_hapvida_com_br/EU2yV3mq7WZLpRp-Kvi5q7EBVLrWJ-cHvcVEk6lWyRktyA?e=psXYZR" "https://smallpdf.com/pt/pdf-para-jpg#r=result&t=2f9355e0e7e9e6968f1c3bee3b0c38e2&i=convert-to-image"
 
 REM Abrir Firefox tela do sistema e tabela!
-start firefox  "http://sistemas.hapvida.com.br/planos" "http://sistemas.hapvida.com.br/planos" "http://www3.hapvida.com.br/hapvida/showFpd.php?idcat=74&id=3215"
+start firefox  "http://sistemas.hapvida.com.br/planos" "http://sistemas.hapvida.com.br/planos" "http://sistemas.hapvida.com.br/planos" "http://www3.hapvida.com.br/hapvida/showFpd.php?idcat=74&id=3215"
 
 REM Abrir aplicativos essenciais
 start notepad 
